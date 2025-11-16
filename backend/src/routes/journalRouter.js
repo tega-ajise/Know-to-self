f30e4c76698aa413d13b0dd9d391481dd0eed835
Tega Ajise <tegaajise@Tegas-MacBook-Pro.local>
2025-11-15 20:05:18 -0500

    done with backend for now

--- backend/src/routes/journalRouter.js
+++ backend/src/routes/journalRouter.js
@@ -15,10 +15,10 @@ router.get("/entries/:row_id", getOneJournalEntry);
 
 // Eventually will pass this to middleware that will check if that row id exists first
 /**Something like:
- * const router = expresss.Router()
  * router.use("/:row_id", checkIdExists) or app.use or whichever one
  * router.route("/:row_id").put().delete()
  * **/
+// Right now just says successful even if row doesn't exist
 router.route("/:row_id").put(updateJournalEntry).delete(deleteJournalEntry);
 
 export default router;
